aa9209a0c5c9ec2645a7c378c99028b7a81b628b
Luke Smetham <luke@smetham.dev>
2021-06-14 11:46:32 +0200

    optional initialValue override for the non-controlled value

--- packages/ui/src/Inputs/shared/useControlledValue.js
+++ packages/ui/src/Inputs/shared/useControlledValue.js
@@ -1,16 +1,16 @@
 import { useCallback, useRef, useState } from 'react';
 
-export const useControlledValue = ({ controlledValue, valueSelector = 'value', type }) => {
+export const useControlledValue = ({ controlledValue, initialValue = '', valueSelector = 'value', type }) => {
 	const { current: isControlled } = useRef(controlledValue !== undefined);
 	
 	// eslint-disable-next-line no-nested-ternary
-	const [internalValue, setValue] = useState(isControlled ? controlledValue : valueSelector === 'checked' ? false : '');
+	const [internalValue, setValue] = useState(isControlled ? controlledValue : valueSelector === 'checked' ? false : initialValue);
 	const value = isControlled ? controlledValue : internalValue;
 
 	const setInternalValue = useCallback((event) => {
 		if (!isControlled) {
 			if (type === 'toggle') {
-				setValue(event.target[valueSelector] === 'on');
+				setValue(event.target[valueSelector]);
 			} else {
 				setValue(event.target[valueSelector]);
 			}
