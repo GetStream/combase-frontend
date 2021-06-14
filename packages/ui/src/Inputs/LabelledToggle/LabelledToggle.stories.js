ba9305b472d5a4395d5703b4cc82281492c63b6b
Luke Smetham <luke@smetham.dev>
2021-06-14 16:15:40 +0200

    Update LabelledToggle.stories.js

--- packages/ui/src/Inputs/LabelledToggle/LabelledToggle.stories.js
+++ packages/ui/src/Inputs/LabelledToggle/LabelledToggle.stories.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {Switch} from '../Switch';
 import LabelledToggle from './LabelledToggle';
 
-export const Default = () => <LabelledToggle label="Two Factor Authentication" />;
+export const WithCheckbox = () => <LabelledToggle label="Two Factor Authentication" />;
 export const WithSwitch = () => <LabelledToggle Input={Switch} inputSize={2} label="Notification Sounds" />;
 
 export default {
