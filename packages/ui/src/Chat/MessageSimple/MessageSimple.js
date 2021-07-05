f62a8e37d3b1a098fcd8f53efd84b6c7e05f728a
Luke Smetham <luke@smetham.dev>
2021-07-05 15:42:19 +0200

    Added attachment styling for the Widget Message component

--- packages/ui/src/Chat/MessageSimple/MessageSimple.js
+++ packages/ui/src/Chat/MessageSimple/MessageSimple.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import format from 'date-fns/format';
-import { useMessageContext } from 'stream-chat-react';
-
+import { useComponentContext, useMessageContext } from 'stream-chat-react';
+// import 'stream-chat-react/dist/css/index.css';
 import Box from '../../Box';
 import Container from '../../Container';
 import Text from '../../Text';
 
 import { SystemMessage } from '../SystemMessage';
-import { MessageAttachments } from '../MessageAttachments';
 import { MessageBubble } from './MessageBubble';
 import { MessageMeta, Root as MessageMetaRoot } from './MessageMeta';
 
@@ -64,14 +63,106 @@ const Root = styled(Container)`
     ${({ $grouping }) => rootStyles[$grouping]?.() || ''};
 `;
 
-const MessageSimple = React.memo(({ index }) => {
-    const { 
+const AttachmentWrapper = styled(Box)`
+	.str-chat__message-attachment {
+		overflow: hidden;
+		width: 100%;
+		max-width: 375px;
+		border-radius: 16px;
+		${'' /* margin: 8px auto 8px 0; */}
+		padding: 0;
+	}
+
+	.str-chat__message-attachment--image {
+		padding: 8px 0;
+		max-width: 480px;
+		height: auto;
+		max-height: 150px;
+		max-width: 100%;
+		cursor: zoom-in;
+
+		& img {
+			border-radius: 16px;
+			height: inherit;
+			width: auto;
+			max-height: inherit;
+			max-width: 100%;
+			display: block;
+			object-fit: cover;
+			overflow: hidden;
+		}
+	}
+
+	.str-chat__gallery {
+		padding: 5px 0;
+		display: inline-flex;
+		flex-wrap: wrap;
+		justify-content: flex-end;
+		overflow: hidden;
+	}
+
+	.str-chat__gallery--square {
+		max-width: 304px;
+		.str-chat__gallery-image:nth-child(even) {
+			margin-right: 0;
+		}
+	}
+
+	.str-chat__gallery-image {
+		width: 150px;
+		height: 150px;
+		background: white;
+		${'' /* margin-bottom: 1px;
+		margin-right: 1px; */}
+	}
+
+	.str-chat__gallery-image img {
+		width: inherit;
+		height: inherit;
+		object-fit: cover;
+	}
+
+	.str-chat__gallery-placeholder {
+		position: relative;
+		width: 150px;
+		height: 150px;
+		color: white;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-size: cover;
+		background-position: top left;
+		background-repeat: no-repeat;
+		cursor: zoom-in;
+
+		:after {
+			content: '';
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			background-color: rgba(0, 0, 0, 0.69);
+			z-index: 0;
+		}
+	}
+`;
+
+const MessageSimple = React.memo(() => {
+    const msgContext = useMessageContext();
+
+	const { 
 		editing,
 		groupStyles: [grouping = 'single'] = [], 
 		isMyMessage, 
 		message,
 		clearEditingState,
-	} = useMessageContext();
+	} = msgContext;
+
+	const {
+		Attachment,
+		EditMessageInput,
+	} = useComponentContext();
 
 	const isOwned = isMyMessage();
 	const type = message?.type;
@@ -84,9 +175,9 @@ const MessageSimple = React.memo(({ index }) => {
             data-grouping={grouping}
         >
             {message?.attachments?.length ? (
-                <Box paddingBottom={1}>
-                    <MessageAttachments message={message} />
-                </Box>
+                <AttachmentWrapper paddingBottom={1}>
+                    <Attachment attachments={message.attachments} />
+                </AttachmentWrapper>
             ) : null}
             {!message?.command && message?.text ? (
                 <MessageBubble $grouping={grouping} $ours={isOwned} className={grouping}>
