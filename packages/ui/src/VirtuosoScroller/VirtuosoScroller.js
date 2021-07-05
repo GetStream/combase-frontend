e722021399ed1941984300fdb3a05757d49ab395
Luke Smetham <luke@smetham.dev>
2021-07-05 15:43:54 +0200

    Update VirtuosoScroller.js

--- packages/ui/src/VirtuosoScroller/VirtuosoScroller.js
+++ packages/ui/src/VirtuosoScroller/VirtuosoScroller.js
@@ -1,12 +1,38 @@
 import React, { forwardRef } from 'react';
+import styled from 'styled-components';
 import { Scrollbars } from 'rc-scrollbars'; 
+import Box from '../Box';
 
-const VirtuosoScroller = forwardRef(({ children, onScroll, ...rest}, ref) => {
+const style = {
+	WebkitOverflowScrolling: "touch",
+	backfaceVisibility: "hidden",
+	height: "100%",
+	outline: "none",
+	overflowY: "hidden",
+	position: "relative",
+};
+
+const Track = styled(Box)`
+	position: absolute;
+	top: ${({ theme }) => theme.space[3]};
+	right: ${({ theme }) => theme.space[1]};
+	bottom: ${({ theme }) => theme.space[3]};
+	display: none;
+
+	@media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+		display: block;
+	}
+`;
+
+const VirtuosoScroller = forwardRef(({ children, onScroll, tabIndex }, ref) => {
 	return (
 		<Scrollbars
 			autoHide
 			autoHideTimeout={3000}
 			autoHideDuration={500}
+			style={style}
+			tabIndex={tabIndex}
+			renderThumbVertical={() => <Box borderRadius={4} backgroundColor="altText" backgroundColorAlpha={0.56} width="0.25rem" />}
 			ref={
 				(sRef) => {
 					if (ref && sRef) {
@@ -19,7 +45,6 @@ const VirtuosoScroller = forwardRef(({ children, onScroll, ...rest}, ref) => {
 					}
 				}
 			} 
-			{...rest}
 			onScrollFrame={onScroll}
 		>	
 			{children}
