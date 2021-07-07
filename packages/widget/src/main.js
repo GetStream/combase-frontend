d5b25615534f9e57f93bfd3e7cc3369ae24f427d
Luke Smetham <luke@smetham.dev>
2021-07-07 16:23:36 +0200

    Update main.js

--- packages/widget/src/main.js
+++ packages/widget/src/main.js
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useChatContext } from 'stream-chat-react';
 
-// import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
-
 import GlobalStyle from './globalStyle';
 
 import { WidgetConfig } from './WidgetConfig';
@@ -13,36 +11,9 @@ import { WidgetShell } from './WidgetShell';
 import Conversation from './views/Conversation';
 import Home from './views/Home';
 
-// import routes from './routes';
-
-// const renderRoute = (route, i) => {
-//     const Layout = route.layout || Fragment;
-//     const Guard = route.guard || Fragment;
-//     const Component = route.component;
-
-//     return (
-//         <Route
-//             key={route.path}
-//             path={route.path}
-//             exact={route.exact}
-//             render={props => (
-//                 <Guard>
-//                     <Layout>{route.routes ? renderRoute(route.routes) : <Component {...props} />}</Layout>
-//                 </Guard>
-//             )}
-//         />
-//     );
-// };
-
-// const widgetRoutes = routes.map(renderRoute);
-
 const Router = () => {
 	const { channel } = useChatContext();
-	return !channel ? (
-		<Home />
-	) : (
-		<Conversation />
-	);
+	return !channel ? <Home /> : <Conversation />;
 };
 
 const CombaseWidget = ({ fabSize, organization, theme }) => (
