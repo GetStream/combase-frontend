b0a96b881dd9e0b6a1395fe28eb8ca4c1487a7b3
Luke Smetham <luke@smetham.dev>
2021-07-30 18:33:08 +0100

    added widget playground

---
+++ packages/widget-playground/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CombaseWidget from '@combase.app/widget/src/main';
+
+function App() {
+  return (
+    <div className="App">
+	  <CombaseWidget organization="60b0c9215716b8022249ed7e" theme="light" />
+    </div>
+  )
+}
+
+export default App
